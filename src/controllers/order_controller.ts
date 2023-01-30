5a26890982d101804257c3adab75eb5ec8045703
Harald <Harald@espinagar.se>
2023-01-30 14:06:04 +0100

    get all orders and get a single order and its order items

---
+++ src/controllers/order_controller.ts
@@ -0,0 +1,34 @@
+import Debug from 'debug'
+import { Request, Response } from 'express'
+import { validationResult } from 'express-validator'
+import prisma from '../prisma'
+
+
+const debug = Debug('prisma-candy-api:order_controller')
+
+// GET ALL ORDERS
+export const index = async (req: Request, res: Response) => {
+    try {
+        const orders = await prisma.order.findMany()
+        return orders
+    } catch (err) {
+        res.status(500).send({ message: "Something went wrong"})
+    }
+}
+
+// GET A SINGLE ORDER AND IT'S ORDER ITEMS
+export const show = async (req: Request, res: Response) => {
+    try {
+        const order = await prisma.order.findUniqueOrThrow({
+            where: {
+                id: id,
+            },
+            include: {
+                items: true
+            },
+        })
+        return order
+    } catch (err) {
+        res.status(500).send({ message: "Something went wrong"})
+    }
+}
