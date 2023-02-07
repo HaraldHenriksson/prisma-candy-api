582dad9d28f8caae762e8b6e7fe70825354b9f8f
Harald <Harald@espinagar.se>
2023-02-07 20:30:46 +0100

    changed back order_rules

--- src/validations/order_rules.ts
+++ src/validations/order_rules.ts
@@ -5,11 +5,11 @@
 import { body } from 'express-validator'
 
 export const orderProductRules = [
-    body('customer_first_name').isString().withMessage('has to be a string').bail(),
-    body('customer_last_name').isString().withMessage('has to be a string').bail(),
-    body('customer_address').isString().withMessage('has to be a string').bail(),
-    body('customer_postcode').isString().isLength({ min: 4, max: 6 }).withMessage('Has to be 4-6 chars long').bail(),
-    body('customer_city').isString().withMessage('has to be a string').bail(),
+    body('customer_first_name').isString(),
+    body('customer_last_name').isString(),
+    body('customer_address').isString(),
+    body('customer_postcode').isString().isLength({ min: 4, max: 6 }).withMessage('Has to be 4-6 chars long'),
+    body('customer_city').isString(),
     body('customer_email').isEmail(),
     body('order_total').isInt({ min: 1 }),
 ]
